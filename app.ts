61a1872970007ee316159d6ceccbfa5fd6d82945
mhmadalsal999-ctrl <mhmadalsal999@gmail.com>
2026-03-25 21:10:58 +0200

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,34 +1,20 @@
-import express, { type Express } from "express";
+import express from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
-import router from "./routes/index.js";
 import { logger } from "./lib/logger.js";
 import { bot } from "./bot/index.js";
+import router from "./routes/index.js";
 
-const app: Express = express();
+const app = express();
 
-app.use(
-  pinoHttp({
-    logger,
-    serializers: {
-      req(req) {
-        return {
-          id: req.id,
-          method: req.method,
-          url: req.url?.split("?")[0],
-        };
-      },
-      res(res) {
-        return {
-          statusCode: res.statusCode,
-        };
-      },
-    },
-  }),
-);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Request logging
+app.use((req, _res, next) => {
+  logger.info({ method: req.method, url: req.url }, "Request");
+  next();
+});
 
 const BOT_TOKEN = process.env["TELEGRAM_BOT_TOKEN"];
 if (BOT_TOKEN) {
